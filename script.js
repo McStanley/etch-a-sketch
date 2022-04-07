f472f70aa6ead169ae5f2bed8a46df65707f310f
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2022-04-07 15:25:39 +0200

    Add function creating a grid
    Add function creating a grid of a specified size.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const grid = document.querySelector('#grid');
+
+function createGrid(gridSize) {
+    grid.replaceChildren();
+    grid.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
+
+    let gridSquares = gridSize ** 2;
+    while (gridSquares) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        grid.appendChild(gridItem);
+        gridSquares--;
+    }
+}
+
+let gridSize = 16;
+
+createGrid(gridSize);

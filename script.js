9b44128310e809e5e455b860326d977ec1b23adf
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2022-04-10 18:18:34 +0200

    Add draw and erase buttons
    Draw button activates drawing mode.
    Erase button activates eraser mode.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = 'steelblue';
 
 const grid = document.querySelector('#grid');
+const btnDraw = document.querySelector('#btn-draw');
+const btnErase = document.querySelector('#btn-erase');
 const btnClear = document.querySelector('#btn-clear');
 
-function createGrid(gridSize) {
+function activateDraw() {
+    btnErase.classList.remove('btn-active');
+    btnDraw.classList.add('btn-active');
+}
+
+function activateErase() {
+    btnDraw.classList.remove('btn-active');
+    btnErase.classList.add('btn-active');
+}
+
+function createGrid() {
     grid.replaceChildren();
     grid.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
 
@@ -14,6 +27,7 @@ function createGrid(gridSize) {
         grid.appendChild(gridItem);
         gridSquares--;
     }
+    activateDraw();
     activateGrid();
 }
 
@@ -28,17 +42,17 @@ function activateGrid() {
 function fillSquare(event) {
     if (event.buttons != 1) return;
 
-    event.target.style.backgroundColor = 'steelblue';
+    if (btnDraw.classList.contains('btn-active')) {
+        event.target.style.backgroundColor = currentColor;
+    } else {
+        event.target.style.backgroundColor = '';
+    }
 }
 
-function clearGrid() {
-    const gridItems = document.querySelectorAll('.grid-item');
-    gridItems.forEach(gridItem => {
-        gridItem.style.backgroundColor = '';
-    });
-}
-
-btnClear.addEventListener('click', clearGrid);
+btnDraw.addEventListener('click', activateDraw);
+btnErase.addEventListener('click', activateErase);
+btnClear.addEventListener('click', createGrid);
 
 let gridSize = DEFAULT_SIZE;
-createGrid(gridSize);
+let currentColor = DEFAULT_COLOR;
+createGrid();
